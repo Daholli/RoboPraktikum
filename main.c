ab339e99841071f5c771344f74caf68da91a3755
Daholli <christoph.hollizeck@web.de>
2020-08-11 12:13:37 +0200

    taster dazugefügt

--- main.c
+++ main.c
@@ -18,19 +18,20 @@ int main(void) {
 	// Initialisierung ausfuehren
 
 	init();
+    uint8_t sw_alt = 0;
+           DDRC |= (1<<5);
+       
+           while (1) {
+               uint8_t sw= !( PINC & ( 1 << 3 ) );
+               if(sw_alt < sw ) {
+                   PORTC |= (1<<5);
+               } else if ( sw_alt > sw) {
+                   PORTC |= (1<<5);
+               } else {
+                   PORTC &= ~(1<<5);
+               }
+           }
 
-    const uint16_t delay=1000;
-    uint32_t nextEvent=getMsTimer()+delay;
-
-
-    DDRC |= (1<<5);
-
-	while (1) {
-        if(nextEvent<getMsTimer()) {
-            nextEvent += delay;
-            PORTC ^= (1<<5); 
-        }
-	}
 }
 
 
