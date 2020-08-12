b991e9806d2fee4921d2a4b739e08119d9cd42ee
Daholli <christoph.hollizeck@web.de>
2020-08-12 11:35:22 +0200

    changed rising to falling

--- main.c
+++ main.c
@@ -131,7 +131,7 @@ int main(void) {
     uint8_t sw_alt= 0;
     while (1) {
         uint8_t sw= !( PINB & ( 1 << 5 ) );
-        if (sw_alt < sw) {
+        if (sw_alt > sw) {
             keyPress();
         }
         sw_alt=sw;
