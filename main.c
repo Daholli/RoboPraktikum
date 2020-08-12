af67577e66c8d208377047ce6cbc689f81829ccc
Daholli <christoph.hollizeck@web.de>
2020-08-12 12:56:19 +0200

    increased delay

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main(void) {
 	init();
     const uint16_t delay = 2000;
     uint32_t nextEvent = getMsTimer()+delay;
-    const char s[] = "Hello World!\n\r";
+    char s[] = "Hello World!\n\r";
     while(1) {
         if(nextEvent < getMsTimer())
             nextEvent += delay;
