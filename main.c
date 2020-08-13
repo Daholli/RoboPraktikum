391b125adaf96d4c511f8475d0f9de4bf853fc6f
Daholli <christoph.hollizeck@web.de>
2020-08-13 13:14:01 +0200

    increased delay

--- main.c
+++ main.c
@@ -44,7 +44,7 @@ int main(void) {
     uint8_t timer =0;
 
     while(1) {
-        _delay_us(15624);
+        _delay_us(100000);
         adc = getADCValue(k);
         while(timer < 255) {
             if(timer >= adc) {
