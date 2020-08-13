4bbb3b30bb6887bd6ebe7880aeb1e301b094c086
Daholli <christoph.hollizeck@web.de>
2020-08-13 13:40:17 +0200

    PWMHardware first commit

--- main.c
+++ main.c
@@ -28,33 +28,15 @@ int main(void) {
 	init();
     //const uint16_t delay = 1000;
     //uint32_t nextEvent = getMsTimer()+delay;
-    //char buffer[255];
-    DDRB |= (1 << 1);
     
-    DDRD &= ~(1<<1);
-    
-    int k = 0;
-    uint16_t adc;
+    //int k = 0;
+    //uint16_t adc;
     //int threshold = 250;
-    uint8_t timer =0;
 
     while(1) {
-        //_delay_us(100000);
-        adc = getADCValue(k);
-        while(timer < 255) {
-            if(timer >= adc) {
-                clearBit(PORTB, 1);
-            } else {
-                setBit(PORTB, 1);
-            }
-            timer++;
-            //uart_puts("\n\r Timer: ");
-            //uart_puti(timer);
-        }
-        timer =0;
-        //uart_puts("\n\r ADC: ");
-        //uart_puti(adc);
-        //uart_puti(adc);         
+        //adc = getADCValue(k);
+        setPWM(123);
+
     }
 }
 
