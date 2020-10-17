3c4d07969848bc32d119d864ea0de8962c29d956
Daholli <christoph.hollizeck@web.de>
2020-10-17 13:13:43 +0200

    added x and y identifiers to arrays so the controller knows how to differ between the 2

--- drawPic.py
+++ drawPic.py
@@ -10,6 +10,13 @@ convert = str(convert).replace('[', '').replace(']', '')
 
 data = np.fromstring(convert, dtype=int, sep=',')
 data = data.reshape(int(data.shape[0]/2),2)
+data = data.astype(str)
+
+x = np.full((data.shape[0]),"x", dtype="<U11")
+y = np.full((data.shape[0]),"y", dtype="<U11")
+
+data[:,0] = np.char.add(x,data[:,0])
+data[:,1] = np.char.add(y,data[:,1])
 
 class ScaraRobo():
     def __init__(self):
@@ -22,24 +29,24 @@ class ScaraRobo():
 
         try:
             self.ser.open()
-        except Exception, e:
-            print "error open serial port: " + str(e)
+        except Exception as e:
+            print("error open serial port: " + str(e))
             exit()
-        self.ser.write(chr(255));
+        self.ser.write(chr(255))
 
     def setX(self, line):
         self.ser.write(data[line][0])
 
-    def setY(self, line)
+    def setY(self, line):
         self.ser.write(data[line][1])
 
 
     def draw(self, data):
         for i in data.shape[0]:
-            setX(i)
-            setY(i)
+            self.setX(i)
+            self.setY(i)
 
 
 robo=ScaraRobo()
 
-synth.draw(data)
+robo.draw(data)
